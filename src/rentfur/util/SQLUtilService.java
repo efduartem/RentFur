37bf004f47faf7f720daca02c4dc405365bdeadb
FDuarte <FDuarte@DESKTOP-SIHIB6L>
2016-03-13 21:44:38 -0300

    Tabla de Resultados en FurnitureIndex.

---
+++ src/rentfur/util/SQLUtilService.java
@@ -0,0 +1,52 @@
+package rentfur.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author FDuarte
+ */
+public class SQLUtilService {
+    
+    public boolean isUniqueCode(String table, String code){
+        Connection connRentFur = null;
+        PreparedStatement ps;
+        ResultSet rs;
+        boolean valueToReturn = false;
+        try{
+                connRentFur = DbConnectUtil.getConnection();    
+                StringBuilder countQuery = new StringBuilder();
+                countQuery.append("SELECT count(*) from ");
+                countQuery.append(table);
+                countQuery.append(" WHERE code = ? ");
+                System.out.println("countQuery: "+countQuery.toString());
+                ps = connRentFur.prepareStatement(countQuery.toString());
+                ps.setString(1, code);
+                rs = ps.executeQuery();
+                if(rs.next()){
+                    if(rs.getInt(1)==0){
+                        valueToReturn = true;
+                    }
+                }
+                ps.close();
+                rs.close();
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+            System.err.println(e);
+        }finally{
+            try{
+                if(connRentFur != null){
+                    connRentFur.close();
+                }
+            }catch(SQLException sqle){
+                System.err.println(sqle.getMessage());
+                System.err.println(sqle);
+            }
+        }
+        
+        return valueToReturn;
+    }
+}

9b63b6bb421c39fe18a16a6f685c505d165a9bc7
FDuarte <FDuarte@DESKTOP-SIHIB6L>
2016-03-20 11:17:50 -0300

    Cambios en Creacion de Mobiliarios (FurnitureCreate y FurnitureController)

--- src/rentfur/furniture/FurnitureController.java
+++ src/rentfur/furniture/FurnitureController.java
@@ -6,15 +6,21 @@
 
 package rentfur.furniture;
 
+import java.awt.Color;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.text.MaskFormatter;
 import rentfur.util.ComboBoxItem;
 import rentfur.util.DbConnectUtil;
+import rentfur.util.SQLUtilService;
 
 /**
  *
@@ -25,7 +31,7 @@ public class FurnitureController {
     private FurnitureIndex furnitureIndex;
     public final int SUCCESFULLY_SAVED = 0;
     public final int ERROR_IN_SAVED = 1;
-    
+    public final String TABLE_NAME = "furniture";
     
     public FurnitureCreate getFurnitureCreate(){
         if(furnitureCreate == null){
@@ -84,6 +90,77 @@ public class FurnitureController {
         
         return furnitureFamilies;
     } 
+
+    HashMap saveFurniture(String code, String description, String familyId, String unitPrice, String unitCostPrice, String fineAmountPerUnit, String totalStock, String observation) {
+        HashMap mapToReturn = new HashMap();
+        Connection connRentFur = null;
+        PreparedStatement ps;
+        
+        try{
+            mapToReturn.put("status", ERROR_IN_SAVED);
+            mapToReturn.put("message", "");
+            SQLUtilService sqlUtilService = new SQLUtilService();
+            if(code == null || code.equals("")){                
+                mapToReturn.put("message", "El campo codigo es requerido para la creacion del Mobiliriario");
+            }else if(description == null || description.equals("")){
+                mapToReturn.put("message", "El campo Descripcion es requerido para la creacion del Mobiliriario");
+            }else if(unitPrice == null || unitPrice.equals("")){
+                mapToReturn.put("message", "El campo Precio unitario es requerido para la creacion del Mobiliriario");
+            }else if(unitCostPrice == null || unitCostPrice.equals("")){
+                mapToReturn.put("message", "El campo Costo unitario es requerido para la creacion del Mobiliriario");
+            }else if(fineAmountPerUnit == null || fineAmountPerUnit.equals("")){
+                mapToReturn.put("message", "El campo Multa es requerido para la creacion del Mobiliriario");
+            }else if(totalStock == null || totalStock.equals("")){
+                mapToReturn.put("message", "El campo Stock es requerido para la creacion del Mobiliriario");
+            }else if(!sqlUtilService.isUniqueCode(TABLE_NAME, code)){
+                mapToReturn.put("message", "El codigo $code ya existe favor ingresar otro valor");
+                furnitureCreate.setCodeTextFieldColor(Color.red);
+            }else{
+                
+                Locale pyLocale = new Locale("es", "PY");
+                NumberFormat nf = NumberFormat.getInstance(pyLocale);
+                connRentFur = DbConnectUtil.getConnection();
+                
+                StringBuilder furnitureInsertSb = new StringBuilder();
+                furnitureInsertSb.append("INSERT INTO furniture(id, code, description, furniture_family_id, unit_price, total_stock,fine_amount_per_unit, unit_cost_price,  observation)");
+                furnitureInsertSb.append(" VALUES (nextval('furniture_seq'), ?, ?, ?, ?, ?, ?, ?, ?)");
+                
+                ps = connRentFur.prepareStatement(furnitureInsertSb.toString());
+                ps.setString(1, code);
+                ps.setString(2, description);
+                ps.setInt(3, Integer.valueOf(familyId));
+                ps.setDouble(4, Double.valueOf(nf.parse(unitPrice).toString()));
+                ps.setDouble(5, Double.valueOf(nf.parse(totalStock).toString()));
+                ps.setDouble(6, Double.valueOf(nf.parse(fineAmountPerUnit).toString()));
+                ps.setDouble(7, Double.valueOf(nf.parse(unitCostPrice).toString()));
+                ps.setString(8, observation);
+                ps.executeUpdate();
+                ps.close();
+                mapToReturn.put("status", SUCCESFULLY_SAVED);
+                mapToReturn.put("message", "Mobiliriario creada correctamente");
+            }
+            
+        }catch(SQLException | NumberFormatException | ParseException th){
+            System.err.println(th.getMessage());
+            System.err.println(th);
+            mapToReturn.put("message", th.getMessage());
+        }finally{
+            try{
+                if(connRentFur != null){
+                    connRentFur.close();
+                }
+            }catch(SQLException sqle){
+                System.err.println(sqle.getMessage());
+                System.err.println(sqle);
+            }
+        }
+        
+        return mapToReturn;
+    }
+    
+    public void viewClosed(){
+        furnitureCreate = null;
+    }
     
     public void setFurnitureIndexResultsTable(DefaultTableModel furnituresResultDefaultTableModel, boolean searchPressed, String code, String description){
         
@@ -117,6 +194,7 @@ public class FurnitureController {
         }catch(Throwable th){
             System.err.println(th.getMessage());
             System.err.println(th);
+            th.printStackTrace();
         }
     }
     
@@ -124,7 +202,7 @@ public class FurnitureController {
         Connection connRentFur = null;
         PreparedStatement ps;
         ResultSet rs;
-        ArrayList listToReturn = null;
+        ArrayList listToReturn = new ArrayList();
         
         try{
             HashMap resultValuesMap;
@@ -135,7 +213,8 @@ public class FurnitureController {
             while(rs.next()){
                 resultValuesMap = new HashMap();
                 resultValuesMap.put("code", rs.getString("code"));
-                resultValuesMap.put("descripcion", rs.getString("descripcion"));
+                resultValuesMap.put("description", rs.getString("description"));
+                System.out.println(resultValuesMap);
                 listToReturn.add(resultValuesMap);
                 /*if(permisosIdMap.containsKey(rs.getInt("idpermisos"))){
                     fila[1] = Boolean.TRUE;
@@ -149,6 +228,7 @@ public class FurnitureController {
         }catch(SQLException th){
             System.err.println(th.getMessage());
             System.err.println(th);
+            th.printStackTrace();
         }finally{
             try{
                 if(connRentFur != null){
@@ -162,4 +242,6 @@ public class FurnitureController {
         
         return listToReturn;
     }
+    
 }
+
--- src/rentfur/furniture/FurnitureCreate.java
+++ src/rentfur/furniture/FurnitureCreate.java
@@ -6,15 +6,40 @@
 
 package rentfur.furniture;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+import java.util.HashMap;
+import java.util.Locale;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.DocumentFilter;
+import rentfur.util.ComboBoxItem;
+import rentfur.util.NumericTextField;
 
 /**
  *
@@ -27,11 +52,23 @@ public class FurnitureCreate extends JInternalFrame{
     private final JLabel descriptionLabel;
     private final JLabel familyLabel;
     private final JLabel unitPriceLabel;
+    private final JLabel unitCostPriceLabel;
+    private final JLabel fineAmountPerUnitLabel;
+    private final JLabel totalStockLabel;
+    private final JLabel observationLabel;
     private final JTextField codeTextField;
     private final JTextField descriptionTextField;
-    private final JTextField familyTextField;
-    private final JTextField unitPriceTextField;
-    JFormattedTextField numformatted;
+    private final JComboBox familyComboBox;
+    private final NumericTextField unitPriceTextField;
+    private final NumericTextField unitCostPriceTextField;
+    private final NumericTextField fineAmountPerUnitTextField;
+    private final NumericTextField totalStockTextField;
+    private final JTextArea observationTextArea;
+    private final ImageIcon createIconImage;
+    private final JButton saveButton;
+    private final ImageIcon cancelIconImage;
+    private final JButton cancelButton;
+    private DecimalFormat amountFormat;
     
     public FurnitureCreate(FurnitureController furnitureController){
         this.furnitureController = furnitureController;
@@ -41,61 +78,269 @@ public class FurnitureCreate extends JInternalFrame{
         furnitureCreatePanel.setLayout(null);
 
         codeLabel = new JLabel("Codigo:");
-        codeLabel.setBounds(30, 20, 80, 25);
+        codeLabel.setBounds(30, 20, 100, 25);
         furnitureCreatePanel.add(codeLabel);
         
         codeTextField = new JTextField(20);
-        codeTextField.setBounds(120, 20, 160, 25);
+        codeTextField.setBounds(160, 20, 160, 25);
+        codeTextField.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                codeTextField.setForeground(Color.BLACK);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
         furnitureCreatePanel.add(codeTextField);
         
-        descriptionLabel = new JLabel("Descripcion");
-        descriptionLabel.setBounds(30,50, 80, 25);
+        descriptionLabel = new JLabel("Descripcion:");
+        descriptionLabel.setBounds(30,50, 100, 25);
         furnitureCreatePanel.add(descriptionLabel);
         
         descriptionTextField = new JTextField(20);
-        descriptionTextField.setBounds(120, 50, 160, 25);
+        descriptionTextField.setBounds(160, 50, 160, 25);
         furnitureCreatePanel.add(descriptionTextField);
         
-        familyLabel = new JLabel("Familia Mobiliario");
-        familyLabel.setBounds(30,80, 80, 25);
+        familyLabel = new JLabel("Familia:");
+        familyLabel.setBounds(30,80, 100, 25);
         furnitureCreatePanel.add(familyLabel);
         
-        familyTextField = new JTextField();
-        familyTextField.setBounds(120, 80, 160, 25);
-        furnitureCreatePanel.add(familyTextField);
-                
-        unitPriceLabel = new JLabel("Precio Unitario");
-        unitPriceLabel.setBounds(30,120, 80, 25);
+        ComboBoxItem[] familiesComboBox = furnitureController.getFurnitureFamiliesForComboBox();
+        familyComboBox = new JComboBox(familiesComboBox);
+        familyComboBox.setBounds(160, 80, 160, 25);
+        furnitureCreatePanel.add(familyComboBox);
+        
+        unitPriceLabel = new JLabel("Precio Unitario:");
+        unitPriceLabel.setBounds(30,110, 100, 25);
         furnitureCreatePanel.add(unitPriceLabel);
         
-        unitPriceTextField = new JTextField(20); //debe ser solo numeric
-        unitPriceTextField.setBounds(120, 120, 160, 25);
-        unitPriceTextField.addKeyListener(new KeyAdapter() {
-            public void keyTyped(KeyEvent e) {
-                char caracter = e.getKeyChar();
+        amountFormat = new DecimalFormat("#,###");
+        amountFormat.setGroupingUsed(true);
+        amountFormat.setGroupingSize(3);
+        amountFormat.setParseIntegerOnly(true);
+        
+        unitPriceTextField = new NumericTextField(20, amountFormat);
+        unitPriceTextField.setBounds(160, 110, 160, 25);
+        unitPriceTextField.addKeyListener(new KeyListener() {
 
-                // Verificar si la tecla pulsada no es un digito
-                if (((caracter < '0') || (caracter > '9')) && (caracter != '\b' /*corresponde a BACK_SPACE*/)) {
-                e.consume(); // ignorar el evento de teclado
-                }
-            }
-        });
+                     @Override
+                     public void keyTyped(KeyEvent e) {
+                     }
+
+                     @Override
+                     public void keyPressed(KeyEvent e) {
+                     }
+
+                     @Override
+                     public void keyReleased(KeyEvent e) {
+                         update(e);
+                     }
+                     
+                     public void update(KeyEvent e){
+                         String texto = unitPriceTextField.getText();
+                         texto = texto.replaceAll("\\.", "");
+                         if(e.getKeyChar()!=','){
+                            texto = texto.replaceAll(",", ".");
+                            if(!texto.isEmpty()){
+                                unitPriceTextField.setValue(Double.valueOf(texto));
+                            }
+                         }else{
+                             texto = texto.replaceAll(",", ".");
+                         }
+                     }
+                 });
+
         furnitureCreatePanel.add(unitPriceTextField);
         
+        unitCostPriceLabel = new JLabel("Costo Unitario:");
+        unitCostPriceLabel.setBounds(30, 140, 100, 25);
+        furnitureCreatePanel.add(unitCostPriceLabel);
         
         
+        unitCostPriceTextField = new NumericTextField(20, amountFormat);
+        unitCostPriceTextField.setBounds(160, 140, 160, 25);
+        unitCostPriceTextField.addKeyListener(new KeyListener() {
+
+                     @Override
+                     public void keyTyped(KeyEvent e) {
+                     }
+
+                     @Override
+                     public void keyPressed(KeyEvent e) {
+                     }
+
+                     @Override
+                     public void keyReleased(KeyEvent e) {
+                         update(e);
+                     }
+                     
+                     public void update(KeyEvent e){
+                         String texto = unitCostPriceTextField.getText();
+                         texto = texto.replaceAll("\\.", "");
+                         if(e.getKeyChar()!=','){
+                            texto = texto.replaceAll(",", ".");
+                            if(!texto.isEmpty()){
+                                unitCostPriceTextField.setValue(Double.valueOf(texto));
+                            }
+                         }else{
+                             texto = texto.replaceAll(",", ".");
+                         }
+                     }
+                 });
         
+        furnitureCreatePanel.add(unitCostPriceTextField);
+        
+        fineAmountPerUnitLabel = new JLabel("Monto multa:");
+        fineAmountPerUnitLabel.setBounds(30,170, 100, 25);
+        furnitureCreatePanel.add(fineAmountPerUnitLabel);
+        
+        fineAmountPerUnitTextField = new NumericTextField(20, amountFormat);
+        fineAmountPerUnitTextField.setBounds(160, 170, 160, 25);
+        fineAmountPerUnitTextField.addKeyListener(new KeyListener() {
+
+                     @Override
+                     public void keyTyped(KeyEvent e) {
+                     }
+
+                     @Override
+                     public void keyPressed(KeyEvent e) {
+                     }
+
+                     @Override
+                     public void keyReleased(KeyEvent e) {
+                         update(e);
+                     }
+                     
+                     public void update(KeyEvent e){
+                         String texto = fineAmountPerUnitTextField.getText();
+                         texto = texto.replaceAll("\\.", "");
+                         if(e.getKeyChar()!=','){
+                            texto = texto.replaceAll(",", ".");
+                            if(!texto.isEmpty()){
+                                fineAmountPerUnitTextField.setValue(Double.valueOf(texto));
+                            }
+                         }else{
+                             texto = texto.replaceAll(",", ".");
+                         }
+                     }
+                 });
+        
+        furnitureCreatePanel.add(fineAmountPerUnitTextField);
+        
+        totalStockLabel = new JLabel("Stock:");
+        totalStockLabel.setBounds(30,200, 100, 25);
+        furnitureCreatePanel.add(totalStockLabel);
+        
+        totalStockTextField = new NumericTextField(20, amountFormat);
+        totalStockTextField.setBounds(160, 200, 160, 25);
+        totalStockTextField.addKeyListener(new KeyListener() {
+
+                     @Override
+                     public void keyTyped(KeyEvent e) {
+                     }
+
+                     @Override
+                     public void keyPressed(KeyEvent e) {
+                     }
+
+                     @Override
+                     public void keyReleased(KeyEvent e) {
+                         update(e);
+                     }
+                     
+                     public void update(KeyEvent e){
+                         String texto = totalStockTextField.getText();
+                         texto = texto.replaceAll("\\.", "");
+                         if(e.getKeyChar()!=','){
+                            texto = texto.replaceAll(",", ".");
+                            if(!texto.isEmpty()){
+                                totalStockTextField.setValue(Double.valueOf(texto));
+                            }
+                         }else{
+                             texto = texto.replaceAll(",", ".");
+                         }
+                     }
+                 });
+        
+        furnitureCreatePanel.add(totalStockTextField);
+        
+        observationLabel = new JLabel("Observacion:");
+        observationLabel.setBounds(30,230, 100, 25);
+        furnitureCreatePanel.add(observationLabel);
+        
+        observationTextArea = new JTextArea();
+        observationTextArea.setBounds(160, 230, 160, 60);
+        furnitureCreatePanel.add(observationTextArea);
+        
+        
+        createIconImage = new ImageIcon(getClass().getResource("/rentfur/button/image/util/create_24x24.png"));
+        saveButton = new JButton(" Crear", createIconImage);
+        saveButton.setBounds(30, 320, 120, 32);
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveButtonAction(e);
+            }
+        });
+        furnitureCreatePanel.add(saveButton);
+        
+        cancelIconImage = new ImageIcon(getClass().getResource("/rentfur/button/image/util/cancel_24x24.png"));
+        cancelButton = new JButton(" Cancelar", cancelIconImage);
+        cancelButton.setBounds(160, 320, 120, 32);
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cancelButtonAction(e);
+            }
+        });
+        furnitureCreatePanel.add(cancelButton);
         
         add(furnitureCreatePanel);
-        pack();
         
         setIconifiable(true);
         setMaximizable(true);
         setResizable(true);
         setClosable(true);
-        setTitle("Crear Familia de Mobiliario");
-        setBounds(200,100,400,200);
-
+        setTitle("Crear Mobiliario");
+        setBounds(200,100,500,400);
+        pack();
         setVisible(true);
     }
+    
+    private void saveButtonAction(ActionEvent e) {
+        String code = codeTextField.getText();
+        String description = descriptionTextField.getText();
+        ComboBoxItem family = (ComboBoxItem) familyComboBox.getSelectedItem();
+        String familyId = family.getKey();
+        String unitPrice = unitPriceTextField.getText();
+        String unitCostPrice = unitCostPriceTextField.getText();
+        String fineAmountPerUnit = fineAmountPerUnitTextField.getText();
+        String totalStock = totalStockTextField.getText();
+        String observation = observationTextArea.getText();
+        
+        HashMap mapReturn = furnitureController.saveFurniture(code, description, familyId, unitPrice, unitCostPrice,fineAmountPerUnit, totalStock, observation);
+        if((Integer) mapReturn.get("status") == furnitureController.SUCCESFULLY_SAVED){
+            JOptionPane.showMessageDialog(null, mapReturn.get("message"), "", JOptionPane.INFORMATION_MESSAGE);
+        }else if((Integer)mapReturn.get("status") == furnitureController.ERROR_IN_SAVED){
+            JOptionPane.showMessageDialog(null, mapReturn.get("message"), "Atencion", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+    
+    
+    private void cancelButtonAction(ActionEvent e) {
+        this.dispose();
+        furnitureController.viewClosed();
+    }
+    
+    public void setCodeTextFieldColor(Color color){
+        codeTextField.setForeground(color);
+    }
+    
 }
